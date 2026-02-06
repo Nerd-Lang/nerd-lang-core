218578586ade86d21d637d81de88c49312c8dd48
TheDreamer123 <93482793+TheDreamer123@users.noreply.github.com>
2026-02-06 18:21:14 +0000

    Fixed compiling in Linux.
    Conditionally sets the correct flag, `_POSIX_C_SOURCE` to allow the usage of `readlink` when the target is Linux.

--- bootstrap/src/main.c
+++ bootstrap/src/main.c
@@ -7,6 +7,10 @@
  *   nerd parse <file.nerd>                  Parse and dump AST
  */
 
+#ifdef __linux__
+#define _POSIX_C_SOURCE 200809L
+#endif
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
